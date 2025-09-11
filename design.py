00c954c49260ce30ddfffd434b0557f5dfd9be55
Mo Said <63526325+MoSaid1@users.noreply.github.com>
2025-09-11 15:51:51 +0300

    design.py

--- design.py
+++ design.py
@@ -3,47 +3,24 @@ import arabic_reshaper
 from bidi.algorithm import get_display
 from datetime import datetime
 
-# دالة لمعالجة النصوص العربية
 def ar_text(txt):
-    if not txt:  # لو النص فاضي
+    """تهيئة النص العربي ليظهر بشكل صحيح"""
+    if not txt:
         return ""
     return get_display(arabic_reshaper.reshape(str(txt)))
 
-# كلاس تصميم الفاتورة
 class InvoicePDF(FPDF):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # تسجيل خط Graphik Arabic
+        self.add_font("GraphikArabic", "", "fonts/GRAPHIK ARABIC SEMIBOLD.OTF", uni=True)
+
     def header(self):
-        self.set_font("Helvetica", "B", 18)  # استخدم خط أساسي متاح
+        self.set_font("GraphikArabic", "", 18)
         self.cell(0, 10, ar_text("فاتورة"), border=False, ln=True, align="C")
-        self.ln(10)
+        self.ln(5)
 
     def footer(self):
         self.set_y(-15)
-        self.set_font("Helvetica", "I", 8)
-        self.cell(0, 10, ar_text("صفحة ") + str(self.page_no()), align="C")
-
-    def customer_info(self, name, code, address, date=None):
-        self.set_font("Helvetica", "", 12)
-        self.cell(0, 10, ar_text("اسم الحساب: ") + ar_text(name), ln=True)
-        self.cell(0, 10, ar_text("كود الحساب: ") + ar_text(code), ln=True)
-        self.cell(0, 10, ar_text("العنوان: ") + ar_text(address), ln=True)
-        if not date:
-            date = datetime.now().strftime("%d-%m-%Y")
-        self.cell(0, 10, ar_text("التاريخ: ") + ar_text(date), ln=True)
-        self.ln(5)
-
-    def invoice_table(self, data):
-        # العناوين
-        self.set_font("Helvetica", "B", 12)
-        col_widths = [40, 25, 25, 35, 25, 30]
-        headers = ["اسم صنف", "كمية", "تاريخ صلاحية", "سعر اجمالي", "الخصم", "إجمالي القيمة"]
-
-        for i, header in enumerate(headers):
-            self.cell(col_widths[i], 10, ar_text(header), border=1, align="C")
-        self.ln()
-
-        # الصفوف
-        self.set_font("Helvetica", "", 12)
-        for row in data:
-            for i, item in enumerate(row):
-                self.cell(col_widths[i], 10, ar_text(str(item)), border=1, align="C")
-            self.ln()
+        self.set_font("GraphikArabic", "", 8)
+        self.cell(0, 10, ar_text(f"صفحة {self.page_no()}"), align="C")
